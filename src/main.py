122d253c647a088cac6ec030e27cde749c483e1f
Konstantin Kochnev <kostyawhite89@gmail.com>
2023-01-20 11:09:37 +0300

    Add better data models

--- src/main.py
+++ src/main.py
@@ -6,45 +6,46 @@ from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-
-from api.v1 import films, genres, persons
+from api.v1 import films
 from core import config
 from core.logger import LOGGING
 from db import elastic, redis
 
 app = FastAPI(
     title=config.PROJECT_NAME,
-    docs_url='/api/openapi',
-    openapi_url='/api/openapi.json',
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
 )
 
 
-@app.on_event('startup')
+@app.on_event("startup")
 async def startup():
-    redis.redis = await aioredis.from_url(f"redis://{config.REDIS_HOST}:{config.REDIS_PORT}",
-                                          encoding="utf-8", max_connections=20)
-    FastAPICache.init(RedisBackend(redis.redis), prefix="fastapi-cache")
-    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    # redis.redis = await aioredis.create_redis_pool((config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20)
+    redis.redis = await aioredis.from_url(
+        f"redis://{config.REDIS_HOST}:{config.REDIS_PORT}",
+        encoding="utf-8",
+        max_connections=20,
+    )
+    elastic.es = AsyncElasticsearch(
+        hosts=[f"{config.ELASTIC_HOST}:{config.ELASTIC_PORT}"]
+    )
 
 
-@app.on_event('shutdown')
+@app.on_event("shutdown")
 async def shutdown():
-    await redis.redis.close()
+    redis.redis.close()
+    await redis.redis.wait_closed()
     await elastic.es.close()
 
 
 # Подключаем роутер к серверу, указав префикс /v1/films
 # Теги указываем для удобства навигации по документации
-app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
-app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
-app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
+app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(
-        'main:app',
-        host='0.0.0.0',
+        "main:app",
+        host="0.0.0.0",
         port=8000,
     )
