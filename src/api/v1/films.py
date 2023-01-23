f6de025ea387508d2de282c5b629e9da619239d9
Arthur Lakiza <lakiza@gmail.com>
2023-01-23 17:36:11 +0300

    Исправления api/films

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -1,4 +1,5 @@
 import binascii
+from enum import Enum
 from http import HTTPStatus
 from uuid import UUID
 
@@ -38,35 +39,34 @@ class FilmDetails(BaseModel):
     directors: list[Person]
 
 
-class Result(BaseModel):
-    next_page: str
-    result: list[Film]
-
-
 class FilmsList(BaseModel):
     count: int
     next: str | None
     results: list[Film]
 
 
+class FilmsSorting(str, Enum):
+    asc = "imdb_rating"
+    desc = "-imdb_rating"
+
+
 @router.get("/search", response_model=FilmsList)
 async def get_films(
-    query: str = Query(default=..., min_length=3),
-    page_size: int = Query(default=10, alias="page[size]", ge=10, le=100),
-    page_number: int = Query(
-        default=1,
-        alias="page[number]",
-        ge=1,
-        description="Номер страницы, данным перебором можно получить не более 10000 документов"
-    ),
-    page_next: str | None = Query(
-        default=None,
-        alias="page[next]",
-        description="Токен (next) для получения следующей страницы, при использовании игнорируется page[number]"
-    ),
-    film_service: FilmService = Depends(get_film_service),
+        query: str = Query(default=..., min_length=3),
+        page_size: int = Query(default=10, alias="page[size]", ge=10, le=100),
+        page_number: int = Query(
+            default=1,
+            alias="page[number]",
+            ge=1,
+            description="Номер страницы, данным перебором можно получить не более 10000 документов"
+        ),
+        page_next: str | None = Query(
+            default=None,
+            alias="page[next]",
+            description="Токен (next) для получения следующей страницы, при использовании игнорируется page[number]"
+        ),
+        film_service: FilmService = Depends(get_film_service),
 ) -> FilmsList:
-
     search_after = None
     if page_next:
         try:
@@ -83,8 +83,8 @@ async def get_films(
 
 @router.get("/{film_id}", response_model=FilmDetails)
 async def film_details(
-    film_id: UUID, film_service: FilmService = Depends(get_film_service)
-) -> Film:
+        film_id: UUID, film_service: FilmService = Depends(get_film_service)
+) -> FilmDetails:
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
@@ -92,27 +92,34 @@ async def film_details(
     return FilmDetails(**film.dict())
 
 
-@router.get("", response_model=Result)
+@router.get("", response_model=FilmsList)
 async def get_films(
-    genre: str = None,
-    # page: int = Query(default=1, ge=1),
-    per_page: int = Query(default=50, ge=10, le=100),
-    sort_field: str = "imdb_rating",
-    sort_order: str = "desc",
-    search_after: str = Query(default=None, alias="page[next]"),
-    film_service: FilmService = Depends(get_film_service),
-):
-    films, search_after = await film_service.get_films(
-        sort_field=sort_field,
-        sort_order=sort_order,
-        genre=genre,
-        per_page=per_page,
-        search_after=search_after,
-    )
-    if not films:
-        return []
-    films = [
-        Film(id=film.id, title=film.title, imdb_rating=film.imdb_rating)
-        for film in films
-    ]
-    return Result(next_page=search_after, result=films)
+        sort: FilmsSorting | None = None,
+        filter_genre: UUID | None = Query(default=None, alias="filter[genre]"),
+        page_size: int = Query(default=10, alias="page[size]", ge=10, le=100),
+        page_number: int = Query(
+            default=1,
+            alias="page[number]",
+            ge=1,
+            description="Номер страницы, данным перебором можно получить не более 10000 документов"
+        ),
+        page_next: str | None = Query(
+            default=None,
+            alias="page[next]",
+            description="Токен (next) для получения следующей страницы, при использовании игнорируется page[number]"
+        ),
+        film_service: FilmService = Depends(get_film_service),
+) -> FilmsList:
+    search_after = None
+    if page_next:
+        try:
+            search_after = await film_service.b64decode(page_next)
+        except (binascii.Error, JSONDecodeError):
+            raise HTTPException(status_code=422, detail="page[next] not valid")
+
+    movies = await film_service.get_films(sort=sort, search_after=search_after, filter_genre=filter_genre,
+                                          size=page_size, page_number=page_number)
+    if not movies:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
+
+    return FilmsList(**movies.dict())
