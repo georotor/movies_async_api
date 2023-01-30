f6235e80d38f6c67576e227748f9fc854a5182a4
Pavel Krizhepolskiy <pk.pavel@gmail.com>
2023-01-31 00:10:54 +0300

    Подключение новых модулей в сервис для жанров

--- src/services/genre.py
+++ src/services/genre.py
@@ -5,35 +5,52 @@ from fastapi import Depends
 from fastapi_cache.decorator import cache
 
 from db.elastic import get_elastic
+from db_managers.abstract_manager import AbstractDBManager
+from db_managers.es_manager import ESDBManager
+from elastic_requests.bool_query import must_query_factory
 from models.genre import Genre, GenresList
 from services.node import NodeService
 
 
 class GenreService(NodeService):
-    def __init__(self, elastic: AsyncElasticsearch):
-        super().__init__(elastic)
+    """Логика для обработки запросов со стороны API."""
+    def __init__(self, db_manager: AbstractDBManager):
+        super().__init__(db_manager)
         self.Node = Genre
         self.index = 'genres'
 
     @cache()
-    async def get_genres(self, size: int = 10, page_number: int = 1) -> GenresList | None:
+    async def get_genres(
+            self, size: int = 10, page_number: int = 1
+    ) -> GenresList | None:
+        """Метод для получения списка жанров. Пока без дополнительных изысков.
+
+        Args:
+          size: кол-во записей на странице (limit);
+          page_number: номер страницы.
+
+        """
         _sort = [
             {"name.raw": {"order": "asc"}},
             {"id": {"order": "asc"}}
         ]
 
-        docs = await self._get_from_elastic(size=size, page_number=page_number, sort=_sort)
-        if not docs:
+        query_obj = must_query_factory(size=size, page_number=page_number)
+        query_obj.add_sort(_sort)
+
+        models, search_after = await self._get_from_elastic(
+            query=query_obj.body,
+        )
+
+        if not models:
             return None
 
-        return GenresList(
-            count=docs['hits']['total']['value'],
-            results=[Genre(**doc['_source']) for doc in docs['hits']['hits']]
-        )
+        return GenresList(count=len(models), results=models)
 
 
 @lru_cache()
 def get_genre_service(
     elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> GenreService:
-    return GenreService(elastic)
+    es_db_manager = ESDBManager(elastic)
+    return GenreService(es_db_manager)
