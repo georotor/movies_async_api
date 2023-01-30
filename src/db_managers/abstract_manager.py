87217b833178268717e290794127a9aaa234c666
Pavel Krizhepolskiy <pk.pavel@gmail.com>
2023-01-31 00:05:49 +0300

    Вынес работу с БД в модуль db_managers

---
+++ src/db_managers/abstract_manager.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Type
+
+from pydantic import BaseModel
+
+
+class DBManagerError(Exception):
+    """Исключение, вызывается при ошибках в работе AbstractDBManager."""
+
+
+class AbstractDBManager(ABC):
+    """Описание интерфейса для поиска данных в БД. Возвращает данные в виде
+    объектов pydantic. Это позволяет:
+        1) решить проблему с валидацией найденных данных;
+        2) унифицировать выходные данные (не зависеть от типа и версии БД).
+
+    В первую очередь рассчитан на работу с Elastic, но при желании несложно
+    написать реализацию и для других БД.
+
+    """
+
+    @abstractmethod
+    async def get(
+            self, table_name: str, object_id: Any, model: Type[BaseModel]
+    ) -> Optional[BaseModel]:
+        """Поиск объекта в указанный таблице (индексе). Метод должен возвращать
+        один единственный объект.
+
+        Args:
+          table_name: название таблицы (индекса);
+          object_id: уникальный идентификатор, тип данных стоит указать в
+          конкретных реализациях;
+          model: модель pydantic со списком нужных полей.
+
+        Returns:
+            Экземпляр pydantic BaseModel с данными из БД.
+
+        """
+
+    @abstractmethod
+    async def search_all(
+            self,
+            table_name: str,
+            model: Type[BaseModel],
+            query: Any,
+    ) -> tuple[list[Optional[BaseModel]], list]:
+        """Поиск по всем полям указанной таблицы (индекса). Возвращает список
+        объектов и значение search_after.
+
+        Args:
+          table_name: название таблицы (индекса);
+          model: модель pydantic со списком нужных полей;
+          query: сформированное тело запроса;
+
+        Returns:
+            Список моделей pydantic BaseModel с данными из БД и значение
+            search_after - стартовое значение для следующей выдачи (термин из
+            Elastic, но при желании можно реализовать и для SQL).
+
+        """
---
+++ src/db_managers/es_manager.py
@@ -0,0 +1,82 @@
+from typing import Optional, Type
+from uuid import UUID
+
+from elasticsearch import (AsyncElasticsearch, ElasticsearchException,
+                           NotFoundError)
+from pydantic import BaseModel
+
+from db_managers.abstract_manager import AbstractDBManager, DBManagerError
+
+
+class ESDBManager(AbstractDBManager):
+    def __init__(self, elastic: AsyncElasticsearch):
+        """Реализация AbstractDBManager для работы с Elastic. Перехватывает
+        корневое исключение ElasticsearchException и меняет на DBManagerError.
+
+        Args:
+          elastic: инициализированное подключение к БД;
+
+        """
+        self.elastic = elastic
+
+    async def get(
+            self, table_name: str, object_id: UUID, model: Type[BaseModel]
+    ) -> Optional[BaseModel]:
+        """Поиск объекта в указанном индексе. Метод должен возвращать
+        один единственный объект.
+
+        Args:
+          table_name: название индекса;
+          object_id: уникальный идентификатор, в данном случае UUID;
+          model: модель pydantic со списком нужных полей.
+
+        Returns:
+            Экземпляр pydantic BaseModel с данными из БД.
+
+        """
+        try:
+            # Приводим id к строке ради корректной подсветки синтаксиса
+            doc = await self.elastic.get(index=table_name, id=str(object_id))
+            return model(**doc['_source'])
+        except NotFoundError:
+            return None
+        except ElasticsearchException as e:
+            raise DBManagerError('ElasticsearchException: {}'.format(e))
+
+    async def search_all(
+            self,
+            table_name: str,
+            model: Type[BaseModel],
+            query: dict
+    ) -> tuple[list[Optional[BaseModel]], list]:
+        """Реализация абстрактного метода. Поиск по всем полям указанного
+        индекса. Возвращает список объектов и значение search_after.
+
+        Про последнее можно почитать вот тут:
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html
+        https://stackoverflow.com/questions/68127892/how-does-search-after-work-in-elastic-search
+
+        Args:
+          table_name: название индекса;
+          model: модель pydantic со списком нужных полей;
+          query: сформированное тело запроса.
+
+        Returns:
+            Список моделей pydantic BaseModel с данными из БД и значение
+            search_after - стартовое значение для следующей выдачи, не работает
+            без sort.
+
+        """
+        try:
+            docs = await self.elastic.search(index=table_name, body=query)
+            hits = docs['hits']['hits']
+            if hits:
+                models = [model.parse_obj(doc['_source']) for doc in hits]
+                search_after = docs['hits']['hits'][-1].get("sort", [])
+                return models, search_after
+            return [], []
+
+        except NotFoundError:
+            return [], []
+        except ElasticsearchException as e:
+            raise DBManagerError('ElasticsearchException: {}'.format(e))
