67f1ddd27ced357e7aeab62ee38b24c882c8247b
Arthur Lakiza <lakiza@gmail.com>
2023-02-07 19:43:06 +0300

    Убраны дублирующиеся тесты

--- src/tests/functional/src/test_genres.py
+++ src/tests/functional/src/test_genres.py
@@ -5,24 +5,19 @@ from http import HTTPStatus
 pytestmark = pytest.mark.asyncio
 
 
-async def test_genre(make_get_request, es_write_data_genres):
-    genre_id = "120a21cf-9097-479e-904a-13dd7198c1dd"
+@pytest.mark.parametrize(
+    'genre_id, answer',
+    [
+        ('120a21cf-9097-479e-904a-13dd7198c1dd', {'status': HTTPStatus.OK, 'name': 'Adventure'}),
+        ('cadefb3c-948c-4363-9f34-864cbc6d00d0', {'status': HTTPStatus.NOT_FOUND}),
+        ('20a21cf-997-49e-94a-13d7198c1dd', {'status': HTTPStatus.UNPROCESSABLE_ENTITY}),
+    ]
+)
+async def test_genre(make_get_request, es_write_data_genres, genre_id, answer):
     response = await make_get_request(url=f"/api/v1/genres/{genre_id}")
-    assert response.status == HTTPStatus.OK
-    assert response.body["id"] == genre_id
-    assert response.body["name"] == "Adventure"
-
-
-async def test_genre_notfound(make_get_request, es_write_data_genres):
-    genre_id = "cadefb3c-948c-4363-9f34-864cbc6d00d0"
-    response = await make_get_request(url=f"/api/v1/genres/{genre_id}")
-    assert response.status == HTTPStatus.NOT_FOUND
-
-
-async def test_genre_valid_id(make_get_request, es_write_data_genres):
-    genre_id = "20a21cf-997-49e-94a-13d7198c1dd"
-    response = await make_get_request(url=f"/api/v1/genres/{genre_id}")
-    assert response.status == HTTPStatus.UNPROCESSABLE_ENTITY
+    assert response.status == answer['status']
+    if 'name' in answer:
+        assert response.body["name"] == "Adventure"
 
 
 async def test_genres(make_get_request, es_write_data_genres):
@@ -31,21 +26,3 @@ async def test_genres(make_get_request, es_write_data_genres):
     assert response.body["count"] == 26
     assert len(response.body["results"]) == 10
     assert response.body["results"][0]["id"] == "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
-
-
-async def test_genres_page_size(make_get_request, es_write_data_genres):
-    response = await make_get_request(url=f"/api/v1/genres?page[size]=20")
-    assert response.status == HTTPStatus.OK
-    assert response.body["count"] == 26
-    assert len(response.body["results"]) == 20
-    assert response.body["results"][0]["id"] == "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
-
-
-async def test_genres_page_number(make_get_request, es_write_data_genres):
-    response = await make_get_request(
-        url=f"/api/v1/genres?page[number]=2&page[size]=15"
-    )
-    assert response.status == HTTPStatus.OK
-    assert response.body["count"] == 26
-    assert len(response.body["results"]) == 11
-    assert response.body["results"][0]["id"] == "ca88141b-a6b4-450d-bbc3-efa940e4953f"
