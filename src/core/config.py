b2dfb661cba6eed1aa1eaecdf7f4a3e99550a55a
Arthur Lakiza <lakiza@gmail.com>
2023-03-06 18:22:14 +0300

    Fix auth_url in config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,3 @@
-from os import environ
-
 from pydantic import BaseModel, BaseSettings
 
 
@@ -17,13 +15,10 @@ class Settings(BaseSettings):
     redis_port: int = 6379
     cache_expire: int = 300
     logging: Logging = Logging()
-    auth_url: str = environ.get(
-        'AUTH_URL', 'http://127.0.0.1:5000/api/v1/user/is_authenticated'
-    )
+    auth_url: str = 'http://127.0.0.1:5000/api/v1/user/is_authenticated'
 
     class Config:
         env_nested_delimiter = '__'
 
 
 settings = Settings()
-
